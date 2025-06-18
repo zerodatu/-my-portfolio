297affe5d7b6faa9dede6acaf0f1da6ba469cc9c
ziropu <datutarousumile@gmail.com>
2025-06-18 15:35:17 +0900

    中身記載

--- theme.config.js
+++ theme.config.js
@@ -3,11 +3,12 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> © Ze;roP（じろぷ） All rights reserved.
+      <a href="https://x.com/datui2525" target="_blank" rel="noopener noreferrer">X</a>
       <style jsx>{`
         a {
           float: right;
+          margin-left: 1rem;
         }
         @media screen and (max-width: 480px) {
           article {
