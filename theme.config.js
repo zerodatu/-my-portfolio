1384d754af8cdabdffe05e795b61dca01c95905b
ziropu <datutarousumile@gmail.com>
2025-06-19 01:04:40 +0900

    テーマjsを修正

--- theme.config.js
+++ theme.config.js
@@ -4,17 +4,18 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Ze;roP（じろぷ） All rights reserved.
-      <a href="https://x.com/datui2525" target="_blank" rel="noopener noreferrer">X</a>
+      <a
+        href="https://x.com/datui2525"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="X（旧Twitter）プロフィールへ"
+      >
+        X
+      </a>
       <style jsx>{`
         a {
           float: right;
           margin-left: 1rem;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
         }
       `}</style>
     </small>
